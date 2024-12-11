689b5252881244997b00938c74953be93f865235
dr rajeev kumar <abhihomeohall72@gmail.com>
2024-12-11 14:04:09 +0530

    action

---
+++ src/app/(main)/seetask-admmin/SeeTakAdmin.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SeeTakAdmin() {
+  return (
+    <div>SeeTakAdmin</div>
+  )
+}
---
+++ src/app/(main)/seetask-admmin/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function seetaskAdmmin() {
+  return (
+    <div>seetask-admmin</div>
+  )
+}
